b4cc9082f3ca93dc3943a1ab0ab3d187a3985b67
jrpgta85 <116498046+jrpgta85@users.noreply.github.com>
2022-11-15 08:32:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ ZILLOW_URL = "https://www.zillow.com/park-slope-brooklyn-new-york-ny/rentals/?se
     # Boulder, CO "https://www.zillow.com/boulder-co/rentals/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Boulder%2C%20CO%22%2C%22mapBounds%22%3A%7B%22west%22%3A-105.71586162207032%2C%22east%22%3A-104.9124863779297%2C%22south%22%3A39.80992565922314%2C%22north%22%3A40.22640537302215%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A30543%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A482386%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A2400%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D"
     # Sand Franciso "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.91226424169922%2C%22east%22%3A-122.1088889975586%2C%22south%22%3A37.503424909695106%2C%22north%22%3A37.933600960252704%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 
-chrome_driver_path = r"C:\Users\jrpgt\Documents\Development\chromedriver.exe"
+chrome_driver_path = "YOUR PATH TO CHROMEDRIVER.EXE"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -126,9 +126,4 @@ for index in range(len(link_list)):
     except NoSuchElementException:
         pass
 
-# # driver.get(FORM_RESPONSES)
-# #
-# # xcel = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div[2]/div/div[1]/div[1]/div[2]/div[1]/div/div')
-# # xcel.click()
-#
-
+driver.quit()
